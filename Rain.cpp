2f6a552609a7173aecb88dee2023e4db2ae8741c
Rafal Fatula <rfatula12@gmail.com>
2017-02-18 20:02:52 +0100

    Added vector with droplets

--- Rain.cpp
+++ Rain.cpp
@@ -3,8 +3,10 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
 #include "SFML\Graphics.hpp"
 #include "SFML\OpenGL.hpp"
+#include <ctime>
 
 
 int main()
@@ -14,8 +16,12 @@ int main()
 
 	window.setActive(true);
 
-
-
+	std::vector<sf::RectangleShape> rectangles;
+	std::srand(std::time(0));
+	for (int i = 0; i < 100; i++) {
+		rectangles.push_back(sf::RectangleShape(sf::Vector2f((rand()%15)/10.f+1,(rand()%200)/10.f+3)));
+		rectangles[i].setPosition(sf::Vector2f(rand()%800,rand()%600));
+	}
 
 	while (window.isOpen())
 	{
@@ -32,8 +38,26 @@ int main()
 					// adjust the viewport when the window is resized
 					glViewport(0, 0, event.size.width, event.size.height);
 					break;
+
+				case sf::Event::KeyPressed:
+					if (event.key.code == sf::Keyboard::Right) {
+						rectangles.push_back(sf::RectangleShape(sf::Vector2f((rand() % 15) / 10.f + 1, (rand() % 600) / 10.f + 8)));
+					}
+					else if (event.key.code == sf::Keyboard::Left) {
+						rectangles.pop_back();
+					}
+					break;
 			}
 			window.clear(sf::Color::Black);
+
+			/*for (int i = 0; i < rectangles.size(); i++) {
+				if(rectangles[i].getPosition())
+				rectangles[i].move();
+			}*/
+
+			for (int i = 0; i < rectangles.size(); i++) {
+				window.draw(rectangles[i]);
+			}
 			
 			window.display();
 		}
